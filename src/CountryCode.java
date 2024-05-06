05adaab2bff157e43fb37cc73a22c10d6986dbfc
Heydie Zuliansyah <helsarayta@gmail.com>
2024-05-06 17:27:34 +0700

    remove comment

--- src/CountryCode.java
+++ src/CountryCode.java
@@ -1,15 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import java.util.Locale;;
 
 public class CountryCode {
     public static void main(String[] args) {
 
         Locale.getISOCountries(Locale.IsoCountryCode.PART1_ALPHA2).stream().filter(i -> i.equals("CM"));
-//        System.out.println(new ArrayList<>(cm));
+
 
     }
 }
